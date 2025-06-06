e2a22b095b44fd1595a6c14ff0279a853ab4faa6
Thomas Richardson <thomas.richardson1018@gmail.com>
2025-06-05 20:22:59 -0400

    Added Rat Ninja Animation

--- spriteSandbox.py
+++ spriteSandbox.py
@@ -38,17 +38,20 @@ class TestState(GameState):
         super().__init__()
         self.testSprite = Sprite('gfx/gfxSpriteNinjaCornRat.bmp', [80,63])
         self.testSprite.velocity = pg.math.Vector2.from_polar((3,0))
+        self.anim = Animation('gfx/animSpriteNinjaCornRat.bmp',[80,63])
 
         return
 
     def render(self, surface):
         surface.fill((50,50,200))
         self.testSprite.render(surface)
+        self.anim.render(surface,(100,100))
 
         return
 
     def update(self):
         self.testSprite.update()
+        self.anim.update()
         
         return
     #end update
