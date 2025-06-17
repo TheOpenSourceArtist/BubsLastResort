f49a22334a6695d3df85af6c30be00eda6b7ca1a
Thomas Richardson <theopensourceartist@gmail.com>
2025-06-17 11:40:50 -0400

    Added size chart for sprites

--- sandboxThomas.py
+++ sandboxThomas.py
@@ -1,12 +1,20 @@
-import OSA
+from OSA import *
+
+class Thomas(Sprite):
+    def __init__(self):
+        super().__init__("gfx/gfxThomasStanding.bmp",[50,100])
+        
+        return
+    #end __inint__
+#end Thomas
 
 def main() -> None:
     #create a new game
-    game: Game = OSA.Game("Thomas Sandbox",[1200,900],[800,600])
-    game.gameStates[''].bgColor = [22,4,75]
+    game: Game = Game("Thomas Sandbox",[1200,900],[800,600])
+    game.gameStates[''].bgColor = [50,125,250]
     
     #initialize game objects
-    sprtThomas: Sprite = OSA.Sprite("gfx/gfxThomasStanding.bmp",[50,100])
+    sprtThomas: Sprite = Thomas()
     game.gameStates[''].gameObjects.append(sprtThomas)
     
     #run the game
